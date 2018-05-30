c9251e238668e4ce3c5869be5f45731dc1038d90
Ben Parman <benparman@gmail.com>
2018-05-30 10:51:31 -0600

    Added PUT endpoint to blogPostRouter.js, working on DELETE endpoint

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -10,12 +10,16 @@ BlogPosts.create('Post #1', 'this is my first post', 'Ben', 'Tuesday');
 BlogPosts.create('Post #2', 'this is my second post', 'Ben', 'Wednesday');
 BlogPosts.create('Post #3', 'this is my third post', 'Ben', 'Thursday');
 
+/*===========GET============*/
 router.get('/', (req,res) => {
   res.json(BlogPosts.get());
 });
 
+/*===========POST===========*/
 router.post('/', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  /* publishDate is NOT required because the BlogPosts.create() function specifies publishDate as
+  publishDate: publishDate || Date.now(), so if a date is not specified, it will be created!!! */
+  const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -24,8 +28,38 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
+  console.log(`Creating blog post "${req.body.title}"`);
   const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
   res.status(201).json(item);
+});
+
+/*===========PUT============*/
+router.put('/:id', jsonParser, (req,res) => {
+  /* publishDate is NOT required because the BlogPosts.create() function specifies publishDate as
+  publishDate: publishDate || Date.now(), so if a date is not specified, it will be created!!! */
+  const requiredFields = ['title', 'content', 'author', 'id'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body.  Please provide a Title, Content, and Author!`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id} must match!`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post #${req.params.id}, "${req.body.title}"`);
+  const updatedPost = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
 });
 
 module.exports = router;

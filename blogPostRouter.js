a6e74cbfc6de7b17abd3a1d1ac5969454d7f6e80
Ben Parman <benparman@gmail.com>
2018-05-30 09:44:20 -0600

    Added POST endpoint to blogPostRouter.js

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -14,4 +14,18 @@ router.get('/', (req,res) => {
   res.json(BlogPosts.get());
 });
 
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body.  Please provide a Title, Content, and Author!`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
 module.exports = router;
